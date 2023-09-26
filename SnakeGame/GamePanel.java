1a470ca3335e9e30501c6a02c5a33b60d7a20ca5
Jasur <103329480+JKKO0@users.noreply.github.com>
2023-09-26 02:00:52 -0400

    Add files via upload

---
+++ SnakeGame/GamePanel.java
@@ -0,0 +1,242 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    // Constants for screen dimensions and game properties
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+    static final int DELAY = 175;
+
+    // Arrays to store snake body coordinates
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+
+    // Initial snake length and score
+    int bodyParts = 6;
+    int applesEaten;
+
+    // Coordinates for the apple
+    int appleX;
+    int appleY;
+
+    // Initial direction of the snake
+    char direction = 'R'; // R - Right, L - Left, U - Up, D - Down
+
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    JButton restartButton;
+
+    // Constructor for the GamePanel
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+
+        // Create a restart button and add an action listener
+        restartButton = new JButton("Restart");
+        restartButton.setFocusable(false); // Avoid focus issues
+        restartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                restartGame();
+            }
+        });
+
+        // Add the restart button to the panel
+        add(restartButton);
+        restartButton.setVisible(false); // Initially hide the button
+    }
+
+    // Start the game
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    // Restart the game
+    public void restartGame() {
+        // Reset all game variables to their initial state
+        bodyParts = 6;
+        applesEaten = 0;
+        direction = 'R';
+        running = true;
+        x[0] = SCREEN_WIDTH / 2;
+        y[0] = SCREEN_HEIGHT / 2;
+        newApple();
+        restartButton.setVisible(false); // Hide the restart button
+        timer.start();
+        repaint(); // Redraw the game panel
+    }
+
+    // Paint the game components
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    // Draw the game elements
+    public void draw(Graphics g) {
+        if (running) {
+            // Draw the grid
+            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
+
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+
+            g.setColor(Color.red);
+            g.setFont(new Font("Ink Free", Font.BOLD, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
+        } else {
+            gameOver(g);
+        }
+    }
+
+    // Generate a new apple at a random position
+    public void newApple() {
+        appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+    }
+
+    // Move the snake
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
+    }
+
+    // Check if the snake has eaten an apple
+    public void checkApple() {
+        if ((x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
+    }
+
+    // Check for collisions (with walls or itself)
+    public void checkCollisions() {
+        // Checks if head collides with body
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+        // Check if head touches left border
+        if (x[0] < 0) {
+            running = false;
+        }
+        // Check if head touches right border
+        if (x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        // Check if head touches top border
+        if (y[0] < 0) {
+            running = false;
+        }
+        // Check if head touches bottom border
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+
+        if (!running) {
+            timer.stop();
+        }
+    }
+
+    // Display the game over screen
+    public void gameOver(Graphics g) {
+        // Score
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
+        // Game Over text
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 75));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
+        restartButton.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+
